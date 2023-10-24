7c56f40bf804bf2779f2bea621fb15d739dd332d
Steffen Oppel <steffen.oppel@gmail.com>
2023-10-24 10:13:38 +0200

    tried to comment out animation to get move vis to work again
    mysterious error message occurred when trying to run function

--- R/movement_visualisation.R
+++ R/movement_visualisation.R
@@ -159,15 +159,15 @@ movement_visualisation <- function(trackingdata,
                                             value = F)
                                    )
                                  ),
-                                 # Switch to add slider and animation
-                                 fluidRow(
-                                   column(width = 12,
-                                          materialSwitch(
-                                            inputId = "animation",
-                                            label = "Time Animation", 
-                                            value = F)
-                                   )
-                                 ),
+                                 # # Switch to add slider and animation
+                                 # fluidRow(
+                                 #   column(width = 12,
+                                 #          materialSwitch(
+                                 #            inputId = "animation",
+                                 #            label = "Time Animation", 
+                                 #            value = F)
+                                 #   )
+                                 # ),
                                  # Slider to select opacity of background map
                                  fluidRow(
                                    column(width = 12,
@@ -258,7 +258,7 @@ movement_visualisation <- function(trackingdata,
     # Should data be displayed in day-night mode
     input_day_night <- shiny::reactive({if(input$day_night == T){1} else{0}})
     # Should temporal animation be added
-    input_animation <- shiny::reactive({if(input$animation == T){1} else{0}})
+    #input_animation <- shiny::reactive({if(input$animation == T){1} else{0}})
     # Defines map opacity
     input_map_opacity <- shiny::reactive({input$map_slider})
     # Defines data size
@@ -531,23 +531,23 @@ movement_visualisation <- function(trackingdata,
     #})
     
     # Adds temporal animation if selected
-    if (input$animation == T) {
-      leafletProxy("map", data = milvus_track_subset()) %>%
-        leaflet.extras2::addPlayback(data = milvus_track,
-                                     time = "t_",
-                                     icon=makeIcon(iconUrl="https://images.phylopic.org/images/aec14bd0-7666-45e2-8a30-17fdd0c79578/vector.svg",
-                                                   iconWidth=30,
-                                                   iconHeight=18,
-                                                   iconAnchorX=15,
-                                                   iconAnchorY=9),
-                                     options = leaflet.extras2::playbackOptions(color = "firebrick",
-                                                                                fill = "firebrick",
-                                                                                radius = 1,
-                                                                                speed = 10000000,
-                                                                                tickLen=1000*60*60,  ## hourly tick lengths stated in milliseconds
-                                                                                maxInterpolationTime=1000*60*60*5 ## 5 hrs interpolation time
-                                     ))
-    }
+    # if (input$animation == T) {
+    #   leafletProxy("map", data = milvus_track_subset()) %>%
+    #     leaflet.extras2::addPlayback(data = milvus_track,
+    #                                  time = "t_",
+    #                                  icon=makeIcon(iconUrl="https://images.phylopic.org/images/aec14bd0-7666-45e2-8a30-17fdd0c79578/vector.svg",
+    #                                                iconWidth=30,
+    #                                                iconHeight=18,
+    #                                                iconAnchorX=15,
+    #                                                iconAnchorY=9),
+    #                                  options = leaflet.extras2::playbackOptions(color = "firebrick",
+    #                                                                             fill = "firebrick",
+    #                                                                             radius = 1,
+    #                                                                             speed = 10000000,
+    #                                                                             tickLen=1000*60*60,  ## hourly tick lengths stated in milliseconds
+    #                                                                             maxInterpolationTime=1000*60*60*5 ## 5 hrs interpolation time
+    #                                  ))
+    # }
   })
     
     # Creates a warning when no data is available in the selected period
--- R/track_animation.R
+++ R/track_animation.R
@@ -228,46 +228,47 @@ leaflet(options = leafletOptions(zoomControl = F)) %>% #changes position of zoom
 
 ##########################  try visualisation in shiny app ################################
 ## copied from workflow demo
-# #### STEP 1: prepare data - this takes approximately 15 minutes
-# nest_data_input<-data_prep(trackingdata=trackingdata,
-#                            indseasondata=indseasondata,
-#                            latboundary=45,
-#                            longboundary=4,
-#                            broodstart= yday(ymd("2023-05-01")),
-#                            broodend<- yday(ymd("2023-06-01")),
-#                            minlocs=800,
-#                            nestradius=50,
-#                            homeradius=2000,
-#                            startseason=70,
-#                            endseason=175,
-#                            settleEnd = 97,  # end of the settlement period in yday
-#                            Incu1End = 113,   # end of the first incubation phase in yday
-#                            Incu2End = 129,  # end of the second incubation phase in yday
-#                            Chick1End = 152, # end of the first chick phase in yday
-#                            age =10)         # age of individuals for which no age is provided with data 
-# 
-# 
-# #### STEP 2: identify home ranges
-# hr_model<-NestTool::hr_model
-# pred_hr<-predict_ranging(model=hr_model$model,trackingsummary=nest_data_input$summary) # uses the model trained with our data (automatically loaded in the function)
-# 
-# #### STEP 3: identify nests
-# nest_model<-NestTool::nest_model
-# pred_nest<-predict_nesting(model=nest_model$model,trackingsummary=pred_hr) # uses the model trained with our data (automatically loaded in the function)
-# 
-# #### STEP 4: determine outcome
-# succ_model<-NestTool::succ_model
-# pred_succ<-predict_success(model=succ_model$model,nestingsummary=pred_nest, nest_cutoff=succ_model$nest_cutoff) # uses the model trained with our data (automatically loaded in the function)
-# 
-# #### STEP 5: extract weekly movement metrics for manual classification
-# move_metrics<-move_metric_extraction(trackingdata=nest_data_input$movementtrack,
-#                                      nest_locs=nest_data_input$pot_nests, 
-#                                      inddata=pred_succ,
-#                                      uncertainty=0.25,
-#                                      nestradius=50,
-#                                      startseason=70,endseason=175)
+#### STEP 1: prepare data - this takes approximately 15 minutes
+nest_data_input<-data_prep(trackingdata=trackingdata,
+                           indseasondata=indseasondata,
+                           latboundary=45,
+                           longboundary=4,
+                           broodstart= yday(ymd("2023-05-01")),
+                           broodend<- yday(ymd("2023-06-01")),
+                           minlocs=800,
+                           nestradius=50,
+                           homeradius=2000,
+                           startseason=70,
+                           endseason=175,
+                           settleEnd = 97,  # end of the settlement period in yday
+                           Incu1End = 113,   # end of the first incubation phase in yday
+                           Incu2End = 129,  # end of the second incubation phase in yday
+                           Chick1End = 152, # end of the first chick phase in yday
+                           age =10)         # age of individuals for which no age is provided with data
+
+
+#### STEP 2: identify home ranges
+hr_model<-NestTool::hr_model
+pred_hr<-predict_ranging(model=hr_model$model,trackingsummary=nest_data_input$summary) # uses the model trained with our data (automatically loaded in the function)
+
+#### STEP 3: identify nests
+nest_model<-NestTool::nest_model
+pred_nest<-predict_nesting(model=nest_model$model,trackingsummary=pred_hr) # uses the model trained with our data (automatically loaded in the function)
+
+#### STEP 4: determine outcome
+succ_model<-NestTool::succ_model
+pred_succ<-predict_success(model=succ_model$model,nestingsummary=pred_nest, nest_cutoff=succ_model$nest_cutoff) # uses the model trained with our data (automatically loaded in the function)
+
+#### STEP 5: extract weekly movement metrics for manual classification
+move_metrics<-move_metric_extraction(trackingdata=nest_data_input$movementtrack,
+                                     nest_locs=nest_data_input$pot_nests,
+                                     inddata=pred_succ,
+                                     uncertainty=0.25,
+                                     nestradius=50,
+                                     startseason=70,endseason=175)
 
 #### STEP 6: use ShinyApp to inspect all questionable individuals
+#source("NestTool2//R//movement_visualisation.r")
 movement_visualisation(trackingdata=nest_data_input$movementtrack,
                        nest_locs=nest_data_input$pot_nests, 
                        inddata=pred_succ,
