df033bf84bf9f4a840a5cad2013d048adb69f536
Steffen Oppel <steffen.oppel@gmail.com>
2024-02-28 12:36:52 +0100

    split nest detection model into M and F models
    reviewer questioned whether inclusion of sex as variable would work and recommended two separate models for M and F - implemented here

--- R/train_nest_detection.R
+++ R/train_nest_detection.R
@@ -18,7 +18,7 @@
 #'
 #'
 #' @export 
-#' @importFrom dplyr filter mutate arrange rename desc select bind_cols case_when bind_rows
+#' @importFrom dplyr filter mutate arrange rename desc select bind_cols case_when bind_rows group_by
 #' @importFrom ranger ranger
 #' @importFrom stats predict
 #' @importFrom caret confusionMatrix
@@ -64,14 +64,21 @@ if(!("nest" %in% names(trackingsummary))){
 trackingsummary$sex <- factor(trackingsummary$sex, levels = c("m","f"))
 
 
-# SPLIT DATA FRAME RANDOMLY INTO TWO--------------------------------------------
-milvus_id <- unique(trackingsummary$year_id)
-milvus_id_train <- sample(milvus_id, round(length(milvus_id)*train_frac))
+# SPLIT DATA FRAME RANDOMLY INTO TWO WHILE KEEPING SEX RATIO CONSTANT --------------------------------------------
+milvus_id_F <- unique(trackingsummary$year_id[trackingsummary$sex=="f"])
+milvus_id_M <- unique(trackingsummary$year_id[trackingsummary$sex=="m"])
+milvus_id_train_F <- sample(milvus_id_F, round(length(milvus_id_F)*train_frac))
+milvus_id_train_M <- sample(milvus_id_M, round(length(milvus_id_M)*train_frac))
 
-milvus_train <- trackingsummary %>%
-  dplyr::filter(year_id %in% milvus_id_train)
-milvus_test <- trackingsummary %>%
-  dplyr::filter(!year_id %in% milvus_id_train)
+milvus_train_M <- trackingsummary %>%
+  dplyr::filter(year_id %in% milvus_id_train_M)
+milvus_test_M <- trackingsummary %>%
+  dplyr::filter(!year_id %in% milvus_id_train_M)
+
+milvus_train_F <- trackingsummary %>%
+  dplyr::filter(year_id %in% milvus_id_train_F)
+milvus_test_F <- trackingsummary %>%
+  dplyr::filter(!year_id %in% milvus_id_train_F)
 names(milvus_train)
 
 
@@ -88,10 +95,10 @@ if(min(table(trackingsummary$nest))<5){
 ############### LOOP OVER TUNING SETTINGS TO IMPROVE PERFORMANCE ##################
 dim.vars<-length(names(trackingsummary))-10
 tuning.out<-expand.grid(m=seq(1:10),t=c(500,750,100,1500,2000,2500,5000)) %>%
-  dplyr::mutate(oob.error=0)
+  dplyr::mutate(oob.error.M=0,oob.error.F=0)
 for (m in seq(1:10)) {
   for(t in c(500,750,100,1500,2000,2500,5000)){
-    RFtest<-ranger::ranger(nest ~ sex + revisits_day + residence_time_day + age_cy +
+    RFtest_M<-ranger::ranger(nest ~ revisits_day + residence_time_day + age_cy +
                              revisits_night + residence_time_night + 
                              dist_max_day_to_max_night + median_day_dist_to_max_night +
                              relative_dist_max_day_to_max_night+
@@ -99,18 +106,28 @@ for (m in seq(1:10)) {
                              timeIncu1+timeIncu2+timeChick1+ timeChick2+meandayrevisitsBrood+lastvisitDay+maxtimeawayBrood2km+maxtimeawayBrood+tottime_nest+
                              Dist99Chick2+Dist99Settle+Dist99Incu1+Dist99Incu2+Dist99Chick1+
                              MCP95Chick2+MCP95Chick1+MCP95Incu1+MCP95Settle+MCP95Incu2,
-                   data = milvus_train, mtry = m, num.trees = t, replace = T, importance = "permutation", oob.error=T, write.forest=F)
-    tuning.out[tuning.out$t==t & tuning.out$m==m,3] <-RFtest$prediction.error
+                   data = milvus_train_M, mtry = m, num.trees = t, replace = T, importance = "permutation", oob.error=T, write.forest=F)
+    tuning.out[tuning.out$t==t & tuning.out$m==m,3] <-RFtest_M$prediction.error
+    RFtest_F<-ranger::ranger(nest ~ revisits_day + residence_time_day + age_cy +
+                               revisits_night + residence_time_night + 
+                               dist_max_day_to_max_night + median_day_dist_to_max_night +
+                               relative_dist_max_day_to_max_night+
+                               revisitsSettle+revisitsIncu1+revisitsIncu2+revisitsChick1+revisitsChick2+timeSettle+
+                               timeIncu1+timeIncu2+timeChick1+ timeChick2+meandayrevisitsBrood+lastvisitDay+maxtimeawayBrood2km+maxtimeawayBrood+tottime_nest+
+                               Dist99Chick2+Dist99Settle+Dist99Incu1+Dist99Incu2+Dist99Chick1+
+                               MCP95Chick2+MCP95Chick1+MCP95Incu1+MCP95Settle+MCP95Incu2,
+                             data = milvus_train_F, mtry = m, num.trees = t, replace = T, importance = "permutation", oob.error=T, write.forest=F)
+    tuning.out[tuning.out$t==t & tuning.out$m==m,4] <-RFtest_F$prediction.error
   }
 }
 
-tuning.out<-tuning.out %>% dplyr::arrange(oob.error)
+
 
 
 ##### RUN MODEL ##########
 
-
-RF2 <- ranger::ranger(nest ~ sex + revisits_day + residence_time_day + age_cy +
+tuning.out<-tuning.out %>% dplyr::arrange(oob.error.M)
+RF2M <- ranger::ranger(nest ~ revisits_day + residence_time_day + age_cy +
                         revisits_night + residence_time_night + 
                         dist_max_day_to_max_night + median_day_dist_to_max_night +
                         relative_dist_max_day_to_max_night+
@@ -118,62 +135,119 @@ RF2 <- ranger::ranger(nest ~ sex + revisits_day + residence_time_day + age_cy +
                         timeIncu1+timeIncu2+timeChick1+ timeChick2+meandayrevisitsBrood+lastvisitDay+maxtimeawayBrood2km+maxtimeawayBrood+tottime_nest+
                         Dist99Chick2+Dist99Settle+Dist99Incu1+Dist99Incu2+Dist99Chick1+
                         MCP95Chick2+MCP95Chick1+MCP95Incu1+MCP95Settle+MCP95Incu2,
-              data=milvus_train, mtry=tuning.out$m[1], num.trees=tuning.out$t[1], replace=T, importance="permutation", oob.error=T, write.forest=T, probability=T)
+              data=milvus_train_M, mtry=tuning.out$m[1], num.trees=tuning.out$t[1], replace=T, importance="permutation", oob.error=T, write.forest=T, probability=T)
 
-IMP<-as.data.frame(RF2$variable.importance) %>%
-  dplyr::mutate(variable=names(RF2$variable.importance)) %>%
-  dplyr::rename(red.accuracy=`RF2$variable.importance`) %>%
+tuning.out<-tuning.out %>% dplyr::arrange(oob.error.F)
+RF2F <- ranger::ranger(nest ~ revisits_day + residence_time_day + age_cy +
+                        revisits_night + residence_time_night + 
+                        dist_max_day_to_max_night + median_day_dist_to_max_night +
+                        relative_dist_max_day_to_max_night+
+                        revisitsSettle+revisitsIncu1+revisitsIncu2+revisitsChick1+revisitsChick2+timeSettle+
+                        timeIncu1+timeIncu2+timeChick1+ timeChick2+meandayrevisitsBrood+lastvisitDay+maxtimeawayBrood2km+maxtimeawayBrood+tottime_nest+
+                        Dist99Chick2+Dist99Settle+Dist99Incu1+Dist99Incu2+Dist99Chick1+
+                        MCP95Chick2+MCP95Chick1+MCP95Incu1+MCP95Settle+MCP95Incu2,
+                      data=milvus_train_F, mtry=tuning.out$m[1], num.trees=tuning.out$t[1], replace=T, importance="permutation", oob.error=T, write.forest=T, probability=T)
+
+
+
+IMP_M<-as.data.frame(RF2M$variable.importance) %>%
+  dplyr::mutate(variable=names(RF2M$variable.importance)) %>%
+  dplyr::rename(red.accuracy=`RF2M$variable.importance`) %>%
   dplyr::arrange(dplyr::desc(red.accuracy)) %>%
   dplyr::mutate(rel.imp=(red.accuracy/max(red.accuracy))*100) %>%
-  dplyr::select(variable,red.accuracy,rel.imp)
+  dplyr::select(variable,red.accuracy,rel.imp) %>%
+  dplyr::mutate(sex="Male")
+
+IMP_F<-as.data.frame(RF2F$variable.importance) %>%
+  dplyr::mutate(variable=names(RF2F$variable.importance)) %>%
+  dplyr::rename(red.accuracy=`RF2F$variable.importance`) %>%
+  dplyr::arrange(dplyr::desc(red.accuracy)) %>%
+  dplyr::mutate(rel.imp=(red.accuracy/max(red.accuracy))*100) %>%
+  dplyr::select(variable,red.accuracy,rel.imp) %>%
+  dplyr::mutate(sex="Female")
 
 
 #### classification success of training data
 
-PRED<-stats::predict(RF2,data=milvus_train, type = "response")
+PRED_M<-stats::predict(RF2M,data=milvus_train_M, type = "response")
+PRED_F<-stats::predict(RF2F,data=milvus_train_F, type = "response")
 
-milvus_train <- milvus_train %>%
+milvus_train_M <- milvus_train_M %>%
   dplyr::rename(nest_observed = nest) %>%
-  dplyr::bind_cols(PRED$predictions) %>%
+  dplyr::bind_cols(PRED_M$predictions) %>%
   dplyr::rename(no_nest_prob = `no nest`, nest_prob = nest) %>%
   dplyr::mutate(nest_predicted = as.factor(dplyr::case_when(nest_prob > no_nest_prob ~ "nest",
                                               nest_prob < no_nest_prob ~ "no nest")))
 
-suppressWarnings({trainmat<-caret::confusionMatrix(data = milvus_train$nest_observed, reference = milvus_train$nest_predicted)})
+suppressWarnings({trainmat_M<-caret::confusionMatrix(data = milvus_train_M$nest_observed, reference = milvus_train_M$nest_predicted)})
+
+milvus_train_F <- milvus_train_F %>%
+  dplyr::rename(nest_observed = nest) %>%
+  dplyr::bind_cols(PRED_F$predictions) %>%
+  dplyr::rename(no_nest_prob = `no nest`, nest_prob = nest) %>%
+  dplyr::mutate(nest_predicted = as.factor(dplyr::case_when(nest_prob > no_nest_prob ~ "nest",
+                                                            nest_prob < no_nest_prob ~ "no nest")))
+
+suppressWarnings({trainmat_F<-caret::confusionMatrix(data = milvus_train_F$nest_observed, reference = milvus_train_F$nest_predicted)})
+
+
+
 
 #### classification success of test data
 
-PRED<-stats::predict(RF2,data=milvus_test, type = "response")
+PRED_M<-stats::predict(RF2M,data=milvus_test_M, type = "response")
+PRED_F<-stats::predict(RF2F,data=milvus_test_F, type = "response")
 
-milvus_test <- milvus_test %>%
+milvus_test_M <- milvus_test_M %>%
   rename(nest_observed = nest) %>%
-  dplyr::bind_cols(PRED$predictions) %>%
+  dplyr::bind_cols(PRED_M$predictions) %>%
   dplyr::rename(no_nest_prob = `no nest`, nest_prob = nest) %>%
   dplyr::mutate(nest_predicted = as.factor(dplyr::case_when(nest_prob > no_nest_prob ~ "nest",
                                     nest_prob < no_nest_prob ~ "no nest")))
 
-suppressWarnings({testmat<-caret::confusionMatrix(data = milvus_test$nest_observed, reference = milvus_test$nest_predicted)})
+suppressWarnings({testmat_M<-caret::confusionMatrix(data = milvus_test_M$nest_observed, reference = milvus_test_M$nest_predicted)})
+
+milvus_test_F <- milvus_test_F %>%
+  rename(nest_observed = nest) %>%
+  dplyr::bind_cols(PRED_F$predictions) %>%
+  dplyr::rename(no_nest_prob = `no nest`, nest_prob = nest) %>%
+  dplyr::mutate(nest_predicted = as.factor(dplyr::case_when(nest_prob > no_nest_prob ~ "nest",
+                                                            nest_prob < no_nest_prob ~ "no nest")))
+
+suppressWarnings({testmat_F<-caret::confusionMatrix(data = milvus_test_F$nest_observed, reference = milvus_test_F$nest_predicted)})
 
 ## export data for further use in outcome prediction
-OUT<-dplyr::bind_rows(milvus_train, milvus_test)
+OUT<-dplyr::bind_rows(milvus_train_M, milvus_train_F, milvus_test_M, milvus_test_F)
 
 
 #### CREATE PLOT FOR VARIABLE IMPORTANCE
+topvars<-dplyr::bind_rows(IMP_M,IMP_F) %>% group_by(sex) %>%
+  dplyr::slice_max(.,order_by=rel.imp,n=10)
+IMP<-dplyr::bind_rows(IMP_M,IMP_F) %>% 
+  dplyr::filter(variable %in% unique(topvars$variable)) %>%
+  dplyr::group_by(sex) %>%
+  dplyr::arrange(sex,desc(rel.imp))
+
 if(plot==T){
-  mylevels<-IMP$variable[10:1]
-  impplot<-IMP[10:1,] %>%
-    dplyr::mutate(variable=forcats::fct_relevel(variable,mylevels)) %>%
-    ggplot2::ggplot(ggplot2::aes(x=variable, y=rel.imp)) +
-    ggplot2::geom_bar(stat='identity', fill='lightblue') +
+  ann_text <- data.frame(rel.imp=80,sex=c("Female","Male"),acc=c(paste("Accuracy = ",round(testmat_F$overall[1],3)),paste("Accuracy = ",round(testmat_M$overall[1],3))))
+  impplot<-IMP %>%
+    dplyr::mutate(variable=forcats::fct_relevel(variable,rev(unique(topvars$variable)))) %>%
+    ggplot2::ggplot(ggplot2::aes(x=variable, y=rel.imp, order = -rel.imp, fill=sex)) +
+    ggplot2::geom_bar(stat='identity', position =  position_dodge2(reverse=TRUE)) +
     ggplot2::coord_flip()+
+    ggplot2::facet_wrap(~sex, ncol=2) +
     ggplot2::ylab("Variable importance (%)") +
     ggplot2::xlab("Explanatory variable") +
     ggplot2::scale_y_continuous(limits=c(-5,105), breaks=seq(0,100,20), labels=seq(0,100,20))+
-    ggplot2::annotate("text",x=2,y=80,label=paste("Accuracy = ",round(testmat$overall[1],3)),size=8) +
+    #ggplot2::annotate("text",x=2,y=80,label=paste("Accuracy = ",round(testmat_M$overall[1],3)),size=8) +
+    ggplot2::geom_text(data=ann_text,mapping = aes(x = 0.6, y = 50, label = acc),hjust= -0.1,vjust = -1) +
     ggplot2::theme(panel.background=ggplot2::element_rect(fill="white", colour="black"), 
                    axis.text.x=ggplot2::element_text(size=18, color="black"),
-                   axis.text.y=ggplot2::element_text(size=16, color="black"), 
-                   axis.title=ggplot2::element_text(size=20), 
+                   axis.text.y=ggplot2::element_text(size=14, color="black"), 
+                   axis.title=ggplot2::element_text(size=20),
+                    legend.position="none",
+                   strip.text=ggplot2::element_text(size=18, color="black"),
+                   strip.background=ggplot2::element_rect(fill="white", colour="black"), 
                    panel.grid.major = ggplot2::element_blank(), 
                    panel.grid.minor = ggplot2::element_blank(), 
                    panel.border = ggplot2::element_blank())
