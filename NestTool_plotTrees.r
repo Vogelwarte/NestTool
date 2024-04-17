5077187725596d410cde46f713a15220bd4a0865
Steffen Oppel <steffen.oppel@gmail.com>
2024-04-17 16:53:48 +0200

    created plots of single classification trees
    requested by a reviewer to show how the model functions - plotted one random tree in each forest

--- NestTool_plotTrees.r
+++ NestTool_plotTrees.r
@@ -17,7 +17,81 @@
 # }
 # for(p in c(cran.packages, 'reprtree')) eval(substitute(library(pkg), list(pkg=p)))
 library(randomForest)
-library(reprtree)
+library(reprtree)  ## instead of loading that package manually define the functions so we can adjust the digits:
+
+labelBG <- function(tr){
+  require(plotrix)
+  charht <- par('cxy')[2L]
+  xy <- tree:::treeco(tr,uniform=TRUE)
+  nodes <- as.integer(row.names(tr$frame))
+  left.child <- match(2*nodes, nodes)
+  rows <- tree:::labels.tree(tr)[left.child]
+  rows <- gsub('NA,','',rows)
+  ind <- !is.na(left.child)
+  boxed.labels(xy$x[ind],xy$y[ind]+0.5*charht, rows[ind] , border=F, bg='white',
+               cex=0.8, xpad=0.5, ypad=1)
+}
+labelYN <- function(tr){
+  charht <- par('cxy')[2L]
+  xy <- tree:::treeco(tr, uniform=T)
+  nodes <- as.integer(row.names(tr$frame))
+  left.child <- match(2*nodes, nodes)
+  ind <- !is.na(left.child)
+  text(xy$x[ind]-0.1, xy$y[ind]-0.2*charht, '<< Y',cex=0.6, adj=1)
+  text(xy$x[ind]+0.1, xy$y[ind]-0.2*charht, 'N >>', cex=0.6, adj=0)
+}
+
+plot.tree <- function (x, y = NULL, type = c("proportional", "uniform"), ...) 
+{
+  if (inherits(x, "singlenode")) 
+    stop("cannot plot singlenode tree")
+  if (!inherits(x, "tree")) 
+    stop("not legitimate tree")
+  type <- match.arg(type)
+  uniform <- type == "uniform"
+  dev <- dev.cur()
+  if (dev == 1L) 
+    dev <- 2L
+  #assign(paste0("device", dev), uniform, envir = tree_env)
+  invisible(treepl(tree:::treeco(x, uniform), node = as.integer(row.names(x$frame)), 
+                   ...))
+}
+
+
+treepl <- function (xy, node, erase = FALSE, ...) 
+{
+  # Modified from tree:::treepl
+  x <- xy$x
+  y <- xy$y
+  parent <- match((node%/%2L), node)
+  sibling <- match(ifelse(node%%2L, node - 1L, node + 1L), 
+                   node)
+  xx <- rbind(x, x, x[sibling], x[sibling], NA)
+  yy <- rbind(y, y[parent], y[parent], y[sibling], NA)
+  if (any(erase)) {
+    lines(c(xx[, erase]), c(yy[, erase]), col = par("bg"))
+    return(x = x[!erase], y = y[!erase])
+  }
+  plot(range(x), c(min(y)-0.5, max(y)+0.5), type = "n", axes = FALSE, xlab = "", 
+       ylab = "")
+  text(x[1L], y[1L], "|", ...)
+  lines(c(xx[, -1L]), c(yy[, -1L]), ...)
+  list(x = x, y = y)
+}
+
+
+plot.getTree <-function (rforest = NULL, k = 1, main = NULL) 
+{
+  gTree <- randomForest::getTree(rforest, k = k, labelVar = TRUE)
+  x <- as.tree(gTree, rforest)
+  x$frame$splits<-substr(x$frame$splits,1,sig.digits+3)
+  plot(x, type = "uniform")
+  text(x, split = FALSE, ...)
+  labelBG(x)
+  labelYN(x)
+  title(main = main)
+}
+
 
 
 ## set root folder for project
@@ -26,7 +100,7 @@ try(setwd("C:/STEFFEN/OneDrive - Vogelwarte/REKI/Analysis/NestTool2"),silent=T)
 
 
 ### LOAD PREPARED TRACKING DATA AND FIT A RANDOM FOREST MODEL 
-trackingsummary<-loadRDS("input_data_prepared.rds")$summary
+trackingsummary<-readRDS("input_data_prepared.rds")$summary
 trackingsummary$nest
 trackingsummary$success
 
@@ -50,8 +124,16 @@ RF1 <- randomForest::randomForest(as.factor(HR) ~ sex + revisits_day + residence
                                     MCP95Chick2+MCP95Chick1+MCP95Incu1+MCP95Settle+MCP95Incu2,
                                   data=trackingsummary, mtry=3, ntree=1500, replace=T, maxnodes=16)
 
-reprtree::plot.getTree(RF1, k=50, main="Example decision tree for the classification of home ranging behaviour", depth=8)
-
+### manually plot the tree
+gTree <- getTree(RF1, k = 50, labelVar = TRUE)
+x <- as.tree(gTree, RF1)
+plot(x, type = "uniform")
+str(x)
+x$frame$splits<-substr(x$frame$splits,1,6)
+text(x, split = FALSE)
+labelBG(x)
+labelYN(x)
+title(main = "Example decision tree for the classification of home ranging behaviour")
 
 RF2 <- randomForest::randomForest(as.factor(nest) ~ sex + revisits_day + residence_time_day + age_cy +
                         revisits_night + residence_time_night + 
@@ -62,19 +144,37 @@ RF2 <- randomForest::randomForest(as.factor(nest) ~ sex + revisits_day + residen
                         Dist99Chick2+Dist99Settle+Dist99Incu1+Dist99Incu2+Dist99Chick1+
                         MCP95Chick2+MCP95Chick1+MCP95Incu1+MCP95Settle+MCP95Incu2,
                       data=trackingsummary, mtry=3, ntree=1500, replace=T, maxnodes=16)
-
-reprtree::plot.getTree(RF2, k=50, main="Example decision tree for the classification of nest initiation", depth=8)
+### manually plot the tree
+gTree <- getTree(RF2, k = 50, labelVar = TRUE)
+x <- as.tree(gTree, RF2)
+plot(x, type = "uniform")
+str(x)
+x$frame$splits<-substr(x$frame$splits,1,6)
+text(x, split = FALSE)
+labelBG(x)
+labelYN(x)
+title(main = "Example decision tree for the classification of nest initiation")
+#reprtree::plot.getTree(RF2, k=50, main="Example decision tree for the classification of nest initiation", depth=8)
 
 
 RF3 <- randomForest::randomForest(as.factor(success) ~ sex + revisits_day + residence_time_day + age_cy +
-                                    revisits_night + residence_time_night + 
+                       revisits_night + residence_time_night + 
                                     dist_max_day_to_max_night + median_day_dist_to_max_night +
-                                    relative_dist_max_day_to_max_night+
+                                 relative_dist_max_day_to_max_night+
                                     revisitsSettle+revisitsIncu1+revisitsIncu2+revisitsChick1+revisitsChick2+timeSettle+
                                     timeIncu1+timeIncu2+timeChick1+ timeChick2+meandayrevisitsBrood+lastvisitDay+maxtimeawayBrood2km+maxtimeawayBrood+tottime_nest+
                                     Dist99Chick2+Dist99Settle+Dist99Incu1+Dist99Incu2+Dist99Chick1+
                                     MCP95Chick2+MCP95Chick1+MCP95Incu1+MCP95Settle+MCP95Incu2,
                                   data=trackingsummary[trackingsummary$success %in% c("yes","no"),], mtry=3, ntree=1500, replace=T, maxnodes=16)
-
-reprtree::plot.getTree(RF3, k=50, main="Example decision tree for the classification of nest success", depth=8)
+### manually plot the tree
+gTree <- getTree(RF3, k = 50, labelVar = TRUE)
+x <- as.tree(gTree, RF3)
+plot(x, type = "uniform")
+str(x)
+x$frame$splits<-substr(x$frame$splits,1,6)
+text(x, split = FALSE)
+labelBG(x)
+labelYN(x)
+title(main = "Example decision tree for the classification of nest success")
+#reprtree::plot.getTree(RF3, k=50, main="Example decision tree for the classification of nest success", depth=8)
 
