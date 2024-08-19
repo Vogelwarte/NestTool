37a30b00b03098a54f515f84e84e7628fe7e5ec3
Steffen Oppel <steffen.oppel@gmail.com>
2024-08-19 17:19:19 +0200

    created German figures for DO-G talk

--- FiguresTables_NestTool.r
+++ FiguresTables_NestTool.r
@@ -203,6 +203,49 @@ ggsave("C:/Users/sop/OneDrive - Vogelwarte/General/MANUSCRIPTS/NestTool/Fig_1.jp
 
 
 
+### DO-G Präsentation Figure:
+
+locale <- Sys.getlocale("LC_TIME")
+
+  on.exit()
+  
+Sys.setlocale("LC_TIME", "German_Germany.1252")
+fig1_move_metrics %>%
+  select(-nest_observed,-nest_prob) %>%
+  dplyr::filter(year_id %in% c("2019_426","2019_28","2022_747")) %>%  ## select three animals at a time
+  dplyr::rename(`Home range size (95% MCP, ha)`= MCP,
+                `Tägliche Flugweite (km)`= median_daydist,
+  ) %>%
+  tidyr::gather(key="MoveMetric",value="Value",-year_id,-week,-age_cy,-sex,-label) %>%
+  dplyr::filter(!is.na(Value)) %>%
+  dplyr::filter(!(label=="uncertain")) %>%
+  dplyr::mutate(label=if_else(label=="breeding", "Brutvogel","Nichtbrüter")) %>%
+  dplyr::filter(MoveMetric=="Tägliche Flugweite (km)") %>%
+  dplyr::mutate(Value=Value/1000) %>%
+  dplyr::mutate(Date=lubridate::dmy(paste0(week, "2020"))) %>%
+  dplyr::ungroup() %>%
+
+
+  ggplot2::ggplot() +
+  ggplot2::geom_point(ggplot2::aes(x = Date, y=Value, color=label), size = 2) +
+  ggplot2::geom_line(ggplot2::aes(x = Date, y=Value, color=label, group=label), linewidth = 1) +
+  ggplot2::facet_wrap(~label) + 
+  
+  ggplot2::labs(y = "Tägliche Flugweite (km)", x = "") +
+  ggplot2::scale_x_date(date_breaks="2 weeks",date_labels=format("%d %b")) +
+  ggplot2::theme(panel.background=ggplot2::element_rect(fill="white", colour="black"),
+                 plot.background=ggplot2::element_rect(fill="white"),
+                 legend.position="none",
+                 panel.grid.major = ggplot2::element_line(colour = "gray70", size = .05),
+                 panel.grid.minor = ggplot2::element_line(colour = "gray70"),
+                 axis.text=ggplot2::element_text(size=10, color="black"),
+                 axis.title=ggplot2::element_text(size=12), 
+                 strip.text=ggplot2::element_text(size=12, color="black"), 
+                 strip.background=ggplot2::element_rect(fill="white", colour="black"))
+Sys.setlocale("LC_TIME", locale)
+
+
+
 
 
 
